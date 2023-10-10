4ff98dafa8d927b633e21ee0c0d156805272cf39
Mohamed Amr <momo.amr.f@gmail.cm>
2023-10-10 20:34:23 +0200

    a

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    n = len(sys.argv) - 1
+    if n == 0:
+        print("{} arguments.".format(n))
+    else:
+        print("{} arguments: ".format(n))
+        for i in range(1, n + 1):
+            print("{}: {}".format(i, sys.argv[i]))
