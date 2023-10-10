ca4c545b52fc675046c4b10f501333cb344a03d8
Mohamed Amr <momo.amr.f@gmail.cm>
2023-10-10 17:34:06 +0200

    a

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,1 +1,10 @@
-print(f'Hello')
+#!/usr/bin/python3
+number = 11
+digit = number % 10
+print(f"Last digit of {number} is {digit}", end=' ')
+if digit > 5:
+	print(f"greater than 5")
+elif digit == 0:
+	print(f"and is 0")
+else:
+	print(f"and is less than 6 and not 0")
