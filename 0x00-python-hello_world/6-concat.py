d0b856457077e3cee4cd18db1fab18f569ad8044
Mohamed Amr <momo.amr.f@gmail.cm>
2023-10-10 11:45:52 +0200

    add file 6

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + ' ' + str2
+print(f"Welcome to {str1}!")
