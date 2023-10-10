d434e43ecee6f3aaac801cb7c0d6b3c2adb68a61
Mohamed Amr <momo.amr.f@gmail.cm>
2023-10-10 20:36:18 +0200

    add file 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
     n = len(sys.argv) - 1
     if n == 0:
         print("{} arguments.".format(n))
+	else if n==1:
+		print("{} argument: ".format(n))
     else:
         print("{} arguments: ".format(n))
         for i in range(1, n + 1):
