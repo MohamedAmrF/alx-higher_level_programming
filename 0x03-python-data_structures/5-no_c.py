e1fe9194af3297520799cc6eb86ceb93c0d53e77
Mohamed Amr <momo.amr.f@gmail.cm>
2023-10-11 09:34:03 +0200

    a

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 def no_c(my_string):
 	c = ""
 	for i in range(0, len(my_string)):
-		if my_string[i] != 'c' adn my_string[i] != 'C'
+		if my_string[i] != 'c' and my_string[i] != 'C'
             c += my_string[i]
 	return c
