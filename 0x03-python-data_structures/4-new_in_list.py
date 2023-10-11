31eded03edfcb6ba3ae7765dfc3541b057f34006
Mohamed Amr <momo.amr.f@gmail.cm>
2023-10-11 09:28:31 +0200

    add file

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
-
-def replace_in_list(my_list, idx, element):
-    newlist = mylist.copy()
-    if idx < 0:
-        return new_list
-    if idx >= len(my_list):
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if idx < 0 or idx >= len(my_list):
         return new_list
     new_list[idx] = element
     return new_list
