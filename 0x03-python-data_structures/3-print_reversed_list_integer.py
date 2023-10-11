352b291afa32f057af4b1b24fc3d8317f610593a
Mohamed Amr <momo.amr.f@gmail.cm>
2023-10-11 09:03:10 +0200

    a

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+
 def print_reversed_list_integer(mylist):
-	for i in range(len(mylist)-1, -1, -1):
-		print("{}".format(mylist[i]))
+    for i in range(len(mylist) - 1, -1, -1):
+        print("{}".format(mylist[i]))
+
 
 if __name__ == "__main__":
-	exit()
+    exit()
